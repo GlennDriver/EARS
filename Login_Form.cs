91e5495899f3b3f3d4ab3552f2f10d90c5310c7b
GlennDriver <39962409+GlennDriver@users.noreply.github.com>
2019-06-20 23:39:20 -0400

    Add files via upload

---
+++ Login_Form.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace EARS
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Amanda Bakalarczyk\source\repos\EARS\EARS\EARS_DB.mdf;Integrated Security=True;Connect Timeout=30");
+            String query = "SELECT * FROM Login_Table WHERE username ='" + txtUsername.Text.Trim() + "' AND password = '" + txtPassword.Text.Trim() + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            adapter.Fill(dtbl);
+
+            if (dtbl.Rows.Count == 1)
+            {
+                Main_Form main = new Main_Form();
+                this.Hide();
+                main.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Incorrect username or password");
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Main_Form.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EARS
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // On click listener for searching 
+        private void searchApplicants_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            Search_Form searchForm = new Search_Form();
+            searchForm.ShowDialog(this);
+            this.Visible = true;
+            this.Close();
+        
+        }
+
+        // On click listener for managing user accounts
+        private void manageUserAccounts_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            ManageUserAccounts_Form manageForm = new ManageUserAccounts_Form();
+            manageForm.ShowDialog(this);
+            this.Visible = true;
+            this.Close();
+
+        }
+
+        // On click listener for managing account settings
+        private void accountSettings_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            AccountSettings_Form accountForm = new AccountSettings_Form();
+            accountForm.ShowDialog(this);
+            this.Visible = true;
+            this.Close();
+
+        }
+    }
+}
